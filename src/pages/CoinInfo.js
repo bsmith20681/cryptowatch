b22fa5718df5c52934757bd75729b1606cf7a7bf
Ben <bsmith20681@gmail.com>
2021-08-02 18:43:15 -0600

    added html parser to coin's description

--- src/pages/CoinInfo.js
+++ src/pages/CoinInfo.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 
 import Layout from "../components/Layout";
 import axios from "axios";
-import NumberFormat from "react-number-format";
 import { Line as LineChart } from "react-chartjs-2";
 import moment from "moment";
+import parse from "html-react-parser";
 
 const CoinInfo = ({ match }) => {
   const [coinPriceHistory, setCoinPriceHistory] = useState("");
+  const [coinDescription, setCoinDescription] = useState("");
   const [loading, setLoading] = useState(true);
   const url = match.params.id;
 
@@ -37,39 +38,61 @@ const CoinInfo = ({ match }) => {
 
   useEffect(() => {
     axios
-      .get(
-        `https://api.coingecko.com/api/v3/coins/${url}/market_chart?vs_currency=usd&days=30&interval=daily`
+      .all([
+        axios.get(
+          `https://api.coingecko.com/api/v3/coins/${url}/market_chart?vs_currency=usd&days=30&interval=daily`
+        ),
+        axios.get(
+          `https://api.coingecko.com/api/v3/coins/${url}?localization=false`
+        ),
+      ])
+      .then(
+        axios.spread((firstResponse, secondResponse) => {
+          setCoinPriceHistory(
+            firstResponse.data.prices.map((price) => price[1].toFixed(2))
+          );
+          setCoinDescription(secondResponse.data);
+          setLoading(false);
+        })
       )
-      .then((res) => {
-        setCoinPriceHistory(
-          res.data.prices.map((price) => price[1].toFixed(2))
-        );
-        setLoading(false);
-      })
       .catch((error) => console.log(error));
   }, []);
 
   return (
     <Layout>
-      <div className="cointainer">
-        <div style={{ maxWidth: 550 }}>
-          {loading ? (
-            "loading"
-          ) : (
-            <LineChart
-              data={data}
-              width={600}
-              height={400}
-              options={{
-                maintainAspectRatio: false,
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                },
-              }}
-            />
-          )}
+      <div className="coinInfo">
+        <div className="container">
+          <div>
+            {loading ? (
+              "loading"
+            ) : (
+              <div>
+                <div style={{ maxWidth: 850, backgroundColor: "#fff" }}>
+                  <LineChart
+                    data={data}
+                    width={600}
+                    height={400}
+                    options={{
+                      maintainAspectRatio: false,
+                      plugins: {
+                        legend: {
+                          display: false,
+                        },
+                        title: {
+                          display: true,
+                          text: `${coinDescription.name}'s Average Price per Month`,
+                        },
+                      },
+                    }}
+                  />
+                </div>
+                <div className="coinInfo-container">
+                  <h3>About {coinDescription.name}</h3>
+                  <div>{parse(coinDescription.description.en)}</div>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </Layout>
